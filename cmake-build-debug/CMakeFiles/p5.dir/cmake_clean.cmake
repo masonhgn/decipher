78a206f676147e3b86a5dc1ac21f21a8ad3086a5
Mason <73012906+masonhgn@users.noreply.github.com>
2021-03-26 19:41:22 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/p5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p5.dir/main.cpp.obj"
+  "libp5.dll.a"
+  "p5.exe"
+  "p5.exe.manifest"
+  "p5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
